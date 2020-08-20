3117609ef70065f12ceaf55ca223269d750ab045
nga.vo <nga.vo>
2020-08-20 14:26:31 +0700

    first commit

--- src/app/order-page/order-page.component.ts
+++ src/app/order-page/order-page.component.ts
@@ -3,6 +3,7 @@ import {Location} from '@angular/common';
 import { Router } from '@angular/router';
 import {Subject} from 'rxjs';
 import {debounceTime} from 'rxjs/operators';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
@@ -10,26 +11,45 @@ import {debounceTime} from 'rxjs/operators';
 })
 export class OrderPageComponent implements OnInit {
   username: string;
-  phone: string;
+  phone: number;
   address: string;
   private _success = new Subject<string>();
   staticAlertClosed = false;
   successMessage = '';
+  registerForm: FormGroup;
+  submitted: boolean = false;
   constructor(
     private _location: Location,
     private router: Router,
+    private formBuilder: FormBuilder
     ) { }
   ngOnInit(): void {
+    
     JSON.parse(localStorage.getItem('brand'));
     setTimeout(() => this.staticAlertClosed = true, 20000);
     this._success.subscribe(message => this.successMessage = message);
     this._success.pipe(debounceTime(2500)
     ).subscribe(() => this.successMessage = '');
+
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      phone: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      address: ['', [Validators.required]],
+  });
   }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
   backClicked() {
     this._location.back();
   }
   order(){
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+        return;
+    }
     if(this.username && this.phone && this.address){
       this.router.navigate([`/notification-page`]);
     }
